44f3fa93ceb950048517e5887dcd422345cd11ec
Adarsh Krishnan <adarshk7@gmail.com>
2013-08-15 16:40:28 -0700

    Procfile fix

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ CSRF_ENABLED = True
 SECRET_KEY = "\xfag}\xedA\xa0\xbd|(\x1b\xec\xaf\xfbx03yx16x00\xd8+x98>\x9f[x05"
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tiger@localhost/wtblog_db'
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
