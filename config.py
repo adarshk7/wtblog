0cd87924940372aac17b3ad0c06d2077cebcb5c4
Adarsh Krishnan <adarshk7@gmail.com>
2013-08-17 09:38:50 -0700

    minor

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = "\xfag}\xedA\xa0\xbd|(\x1b\xec\xaf\xfbx03yx16x00\xd8+x98>\x9f[x05"
 
-SQLALCHEMY_DATABASE_URI = 'postgres://vwzxmmighrncku:pOhPngoOZETCElfaqj_gPdPrGo@ec2-54-221-224-138.compute-1.amazonaws.com:5432/dcm0rnfm01rq1'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
