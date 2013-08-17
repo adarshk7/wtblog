9fc4259f9f859aca176725e6d35d8c410b7b66c9
Adarsh Krishnan <adarshk7@gmail.com>
2013-08-17 09:31:30 -0700

    URL Change for DB Server

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = "\xfag}\xedA\xa0\xbd|(\x1b\xec\xaf\xfbx03yx16x00\xd8+x98>\x9f[x05"
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tiger@localhost/wtblog_db'
+SQLALCHEMY_DATABASE_URI = 'postgres://vwzxmmighrncku:pOhPngoOZETCElfaqj_gPdPrGo@ec2-54-221-224-138.compute-1.amazonaws.com:5432/dcm0rnfm01rq1'
