8495ce1fcacc5d09060c1065b2526f62fc4cc3f5
Adarsh Krishnan <adarshk7@gmail.com>
2013-08-17 10:07:21 -0700

    DB config update

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 CSRF_ENABLED = True
 SECRET_KEY = "\xfag}\xedA\xa0\xbd|(\x1b\xec\xaf\xfbx03yx16x00\xd8+x98>\x9f[x05"
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tiger@localhost/wtblog_db'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
